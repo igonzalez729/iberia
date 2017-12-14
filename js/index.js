62829322fef147e74bcf9d316cb8cb36ab070560
igonzalez729 <32330124+igonzalez729@users.noreply.github.com>
2017-12-14 09:37:18 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,2 +1,55 @@
-var buscador = document.getElementById('buscador').value=valor;
-buscador.innerHTML = buscador.innerHTML.value='';
+
+/*$( window ).resize(function() {
+    $( "body" ).prepend( "<div>" + $( window ).width() + "</div>" );
+  });
+  jQuery.fn.init [Window]0: Window {postMessage: ƒ, blur: ƒ, focus: ƒ, close: ƒ, frames: Window, …}length: 1__proto__: Object(0)
+  console.log($( window ).width())
+  VM418:1 630
+  undefined
+  $( window ).resize(function() {
+    console.log($( window ).width())
+  });
+  jQuery.fn.init [Window]
+--------------------------------------------------------------------------------------*/
+  
+ /* if (matchMedia) {
+    const mq319 = window.matchMedia("(min-width: 319px)");
+    const mq470 = window.matchMedia("(min-width: 470px)");
+    const mq767 = window.matchMedia("(min-width: 767px)");
+    const mq991 = window.matchMedia("(min-width: 991px)");
+    mq319.addListener(WidthChange);
+    WidthChange(mq319);
+    mq470.addListener(WidthChange);
+    WidthChange(mq470);
+    mq767.addListener(WidthChange);
+    WidthChange(mq767);
+    mq991.addListener(WidthChange);
+    WidthChange(mq991);
+  }*/
+  
+  /*// media query change
+  function WidthChange(mq319) {
+    if (mq319.matches) {
+      // window width is at least 319px
+      alert("MQ DENTRO DE 319");
+      console.log('MQ DENTRO DE 319');
+    } else {
+      // window width is less than 319px
+      alert("MQ FUERA DE 319");
+      console.log('MQ fuera DE 319');
+    }
+  
+  }
+
+  function WidthChange(mq319) {
+    if (mq319.matches) {
+      // window width is at least 319px
+      alert("MQ DENTRO DE 319");
+      console.log('MQ DENTRO DE 319');
+    } else if (mq470.matches) {
+      // window width is less than 319px
+      alert("MQ DENTRO DE 470");
+      console.log('MQ DENTRO DE 470');
+    }
+  
+  }*/
